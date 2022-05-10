c9cf504416d928c99ebf4e553a480e799be98c83
Abigail Jones <arjones@sandia.gov>
2022-05-10 15:39:46 -0600

    Refactored into functions

--- iv_curves_prec.py
+++ iv_curves_prec.py
@@ -1,10 +1,8 @@
 import pvlib
 import numpy as np
-from mpmath import *
-from itertools import product
 import matplotlib.pyplot as plt
-
-plt.style.use('seaborn-darkgrid')
+from mpmath import mp
+from itertools import product
 
 
 r"""
@@ -22,48 +20,13 @@ ns -> number of cells in series
 """
 
 
-def diff_lhs_rhs(v, i, il, io, rs, rsh, n, ns, vth):
+def diff_lhs_rhs(v, i, il, io, rs, rsh, n, vth, ns):
     # returns the difference between the lhs and rhs of the single diode equation
     return (il - io*(mp.exp((v + i*rs)/(n*ns*vth)) - 1) - (v + i*rs)/(rsh) - i)
 
 
-# Boltzmann's const (J/K), electron charge (C), temp (K) 
-k, q, temp_cell = [1.380649e-23, 1.60217663e-19, 298.15]
-vth = (k * temp_cell) / q
-
-num_pts = 100
-mp.dps = 20
-tol = 1e-16
-
-
-case1 = True 
-
-if case1:
-    IL = [1.0, 8.0]
-    IO = [5e-10, 3e-8]
-    RS = [0.1, 1.0]
-    RSH = [300, 3000]
-    N = [1.01, 1.3]
-    ns = 72
-else: # we're in case 2
-    IL = [0.5, 2.5] 
-    IO = [1e-9, 1e-8]
-    RS = [0.1, 1.0]
-    RSH = [300, 3000]
-    N = [1.3, 1.5]
-    ns = 140
-
-
-# setting up plot
-plot = plt.plot()
-plt.xlabel('Voltage')
-plt.ylabel('Current')
-if case1: plt.title('Case 1')
-else: plt.title('Case 2')
-
-
-for il, io, rs, rsh, n in product(IL, IO, RS, RSH, N):
-    res = pvlib.pvsystem.singlediode(il, io, rs, rsh, n*ns*vth, num_pts)
+def get_precise_i(il, io, rs, rsh, n, vth, ns, atol, num_pts):
+    res = pvlib.pvsystem.singlediode(il, io, rs, rsh, n*vth*ns, num_pts)
     vv = res['v']
     ii = res['i']
     prec_i = np.zeros_like(ii) # initialize 'empty' array for new, more precise i's
@@ -71,22 +34,68 @@ for il, io, rs, rsh, n in product(IL, IO, RS, RSH, N):
     assert len(vv) == len(ii)
     for idx in range(len(vv)):
         # check if i val already precise enough
-        if abs(diff_lhs_rhs(vv[idx], ii[idx], il, io, rs, rsh, n, ns, vth)) < tol: 
+        if abs(diff_lhs_rhs(vv[idx], ii[idx], il, io, rs, rsh, n, vth, ns)) < atol: 
             new_i = ii[idx]
 
         else:
             # findroot takes the function whose roots we want to find, a good guess for where the root is, and a tolerance
             # default solver uses secant method
-            new_i = findroot(lambda i: diff_lhs_rhs(vv[idx], i, il, io, rs, rsh, n, ns, vth), ii[idx], tol=tol)
+            new_i = mp.findroot(lambda i: diff_lhs_rhs(vv[idx], i, il, io, rs, rsh, n, vth, ns), ii[idx], tol=atol)
 
-        # check that findroot did what we wanted 
-        assert abs(diff_lhs_rhs(vv[idx], new_i, il, io, rs, rsh, n, ns, vth)) < tol
+        # check that mp.findroot did what we wanted 
+        assert abs(diff_lhs_rhs(vv[idx], new_i, il, io, rs, rsh, n, vth, ns)) < atol
 
         # updating array of i's
         prec_i[idx] = new_i
 
-    # graph
-    plot += plt.plot(vv, prec_i)
+    return vv, prec_i
+    # return plt.plot(vv, prec_i)
 
-plt.show()
+
+def plotter(il, io, rs, rsh, n, vth, ns, atol, num_pts, case):
+    # plot a single IV curve 
+    plt.xlabel('Voltage')
+    plt.ylabel('Current')
+
+    if case == 1: plt.title('Case 1')
+    else: plt.title('Case 2') # we're in case 2
+
+    v_vals, i_vals = get_precise_i(il, io, rs, rsh, n, vth, ns, atol, num_pts)
+    return plt.plot(v_vals, i_vals)
+
+
+
+if __name__ == "__main__":
+    mp.dps = 20 # set precision
+    num_pts = 100 
+    atol = 1e-16 
+
+    # Boltzmann's const (J/K), electron charge (C), temp (K) 
+    k, q, temp_cell = [1.380649e-23, 1.60217663e-19, 298.15]
+    vth = (k * temp_cell) / q
+
+    case1 = False 
+    case = [1 if case1 else 2][0]
+
+    if case1:
+        IL = [1.0, 8.0]
+        IO = [5e-10, 3e-8]
+        RS = [0.1, 1.0]
+        RSH = [300, 3000]
+        N = [1.01, 1.3]
+        ns = 72
+    else: # we're in case 2
+        IL = [0.5, 2.5] 
+        IO = [1e-9, 1e-8]
+        RS = [0.1, 1.0]
+        RSH = [300, 3000]
+        N = [1.3, 1.5]
+        ns = 140
+
+    plt.style.use('seaborn-darkgrid')
+    plot = plt.plot()
+    for il, io, rs, rsh, n in product(IL, IO, RS, RSH, N):
+        plot += plotter(il, io, rs, rsh, n, vth, ns, atol, num_pts, case)
+    
+    plt.show()
 
