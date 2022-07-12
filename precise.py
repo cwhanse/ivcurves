3de83326c7c6579c4cb9f49767700b356ba58a74
Taos Transue <spacechips@rocketmail.com>
2022-07-12 12:32:10 -0700

    read case parameters from csvs

--- precise.py
+++ precise.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpmath import mp
 from itertools import product
+import csv
 
 
 def diff_lhs_rhs(v, i, il, io, rs, rsh, n, vth, ns):
@@ -145,50 +146,44 @@ def get_precise_i(il, io, rs, rsh, n, vth, ns, atol, num_pts):
     return vv, prec_i
 
 
-def plotter(il, io, rs, rsh, n, vth, ns, atol, num_pts, case):
+def plotter(il, io, rs, rsh, n, vth, ns, atol, num_pts, case_title):
     # plot a single IV curve 
     plt.xlabel('Voltage')
     plt.ylabel('Current')
 
-    if case == 1: plt.title('Case 1')
-    else: plt.title('Case 2') # we're in case 2
+    plt.title(case_title)
 
     v_vals, i_vals = get_precise_i(il, io, rs, rsh, n, vth, ns, atol, num_pts)
     return plt.plot(v_vals, i_vals)
 
 
+def read_case_parameters(filename):
+    with open(f'{filename}.csv', newline='') as file:
+        reader = csv.DictReader(file, delimiter=',')
+        columns = ['IL', 'IO', 'RS', 'RSH', 'N', 'ns']
+        rows = []
+        for row in reader:
+            rows.append([float(row[col]) for col in columns])
+        return rows
+
 
 if __name__ == "__main__":
     mp.dps = 40 # set precision, 16*2 rounded up
-    num_pts = 100 
-    atol = 1e-16 
+    num_pts = 100
+    atol = 1e-16
 
     # Boltzmann's const (J/K), electron charge (C), temp (K) 
     k, q, temp_cell = [1.380649e-23, 1.60217663e-19, 298.15]
     vth = (k * temp_cell) / q
 
-    case1 = True
-    case = [1 if case1 else 2][0]
-
-    if case1:
-        IL = [1.0, 8.0]
-        IO = [5e-10, 3e-8]
-        RS = [0.1, 1.0]
-        RSH = [300, 3000]
-        N = [1.01, 1.3]
-        ns = 72
-    else: # we're in case 2
-        IL = [0.5, 2.5] 
-        IO = [1e-9, 1e-8]
-        RS = [0.1, 1.0]
-        RSH = [300, 3000]
-        N = [1.3, 1.5]
-        ns = 140
+    case_number = 1
+    case_filename = f'case{case_number}'
+    case_title = f'Case {case_number}'
 
     plt.style.use('seaborn-darkgrid')
     plot = plt.plot()
-    for il, io, rs, rsh, n in product(IL, IO, RS, RSH, N):
-        plot += plotter(il, io, rs, rsh, n, vth, ns, atol, num_pts, case)
+    for il, io, rs, rsh, n, ns in read_case_parameters(case_filename):
+        plot += plotter(il, io, rs, rsh, n, vth, ns, atol, num_pts, case_title)
     
     plt.show()
 
