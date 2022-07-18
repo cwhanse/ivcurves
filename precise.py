e0c9d6a555b94f1718813be1f810b4e210b87fb2
Taos Transue <spacechips@rocketmail.com>
2022-07-18 11:44:26 -0700

    reformatting float strings to always have 16 digits after decimal point

--- precise.py
+++ precise.py
@@ -6,7 +6,7 @@ import json
 import datetime
 import max_power
 import utils
-from utils import mp, diff_lhs_rhs
+from utils import mp, diff_lhs_rhs, mp_nstr_precision_func
 
 
 def get_precise_i(il, io, rs, rsh, n, vth, ns, atol, num_pts):
@@ -134,23 +134,17 @@ def write_case_tests(case_filename, case_parameter_sets, vth, temp_cell, atol,
         i_sc = ii.max()
         v_mp, i_mp, p_mp = max_power.max_power_pt_finder(il, io, rs, rsh, n,
                                                          vth, ns, atol)
-        precision = 16
-        # find max digits to the left of decimal
+
         all_mpf = itertools.chain(vv, ii, [v_oc, i_sc, v_mp, i_mp, p_mp])
-        # force mpf to string in decimal format, no exponent
-        mpf_to_str = lambda x: mp.nstr(x, n=precision*2, min_fixed=-mp.inf,
-                                       max_fixed=mp.inf)
-        ldigits = max(mpf_to_str(abs(num)).find('.') for num in all_mpf)
+        nstr = utils.mp_nstr_precision_func
 
-        nstr16 = lambda x: mp.nstr(x, n=ldigits+precision)
-        vv_str_list = [nstr16(x) for x in vv]
-        ii_str_list = [nstr16(x) for x in ii]
-
+        vv_str_list = [nstr(x) for x in vv]
+        ii_str_list = [nstr(x) for x in ii]
         case_test_suite['IV Curves'].append({
             'Index': test_idx, 'Voltages': vv_str_list,
-            'Currents': ii_str_list, 'v_oc': nstr16(v_oc),
-            'i_sc': nstr16(i_sc), 'v_mp': nstr16(v_mp),
-            'i_mp': nstr16(i_mp), 'p_mp': nstr16(p_mp),
+            'Currents': ii_str_list, 'v_oc': nstr(v_oc),
+            'i_sc': nstr(i_sc), 'v_mp': nstr(v_mp),
+            'i_mp': nstr(i_mp), 'p_mp': nstr(p_mp),
             'Temperature': mp.nstr(temp_cell, n=5), 'Irradiance': None,
             'Sweep direction': None, 'Datetime': None
         })
--- utils.py
+++ utils.py
@@ -1,5 +1,9 @@
+import os
 import csv
 from mpmath import mp
+
+
+TESTS_DIR = 'tests'
 
 
 def set_globals():
@@ -8,6 +12,7 @@ def set_globals():
 
 def constants():
     num_pts = 100
+    precision = 16
     atol = mp.mpmathify(1e-16)
 
     # Boltzmann's const (J/K), electron charge (C), temp (K) 
@@ -15,10 +20,76 @@ def constants():
     vth = (k * temp_cell) / q
     
     return {'k': k, 'q': q, 'temp_cell': temp_cell, 'vth': vth, 'atol': atol,
-            'num_pts': num_pts}
+            'precision': precision, 'num_pts': num_pts}
+
+
+def mp_num_digits_left_of_decimal(num_mpf):
+    r"""
+    Finds the number of digits to the left of an mpmath float's decimal point.
+    If the mpmath float is strictly between -1 and 1, the number of digits
+    is zero.
+
+    Parameters
+    ----------
+    num_mpf : numeric
+        The mpmath float. [-]
+
+    Returns
+    -------
+    int
+        The number of digits to the left of the decimal point of `num_mpf`,
+        ignoring leading zeros.
+    """
+    if abs(num_mpf) < 1:
+        # ignore leading zero
+        return 0
+    else:
+        precision = constants()['precision']
+        # force mpf to string in decimal format, no scientific notation
+        # mpf string will have precision*2 significant digits
+        # all leading zeros are stripped
+        return mp.nstr(num_mpf, n=precision*2, min_fixed=-mp.inf,
+                       max_fixed=mp.inf).find('.')
+
+
+def mp_nstr_precision_func(num_mpf):
+    r"""
+    Converts an mpmath float to a string with 16 significant digits
+    after the decimal place.
+
+    Parameters
+    ----------
+    num_mpf : numeric
+        The mpmath float. [-]
+
+    Returns
+    -------
+    string
+        A string representation of `num_mpf` with 16 siginigcant digits
+        after the decimal place.
+    """
+    precision = constants()['precision']
+    ldigits = mp_num_digits_left_of_decimal(num_mpf)
+    return mp.nstr(num_mpf, n=ldigits+precision, strip_zeros=False)
 
 
 def read_case_parameters(filename):
+    r"""
+    Returns a 2-D list of entries in a CSV file with these column names:
+    Index, photocurrent, saturation_current, resistance_series,
+    resistance_shunt, n, and cells_in_series.
+
+    Parameters
+    ----------
+    filename : string
+        The path to the CSV file excluding the file extension.
+
+    Returns
+    -------
+    2-D list
+        The 2-D list stores every value in the CSV file as a mpmath float,
+        except values in the Index column which are stored as integers.
+    """
     with open(f'{filename}.csv', newline='') as file:
         reader = csv.DictReader(file, delimiter=',')
         parameter_columns = ['photocurrent', 'saturation_current',
@@ -32,6 +103,25 @@ def read_case_parameters(filename):
                     for col in parameter_columns]
             )
         return rows
+
+
+def case_filenames():
+    """
+    Returns a sorted list of filenames in the directory `TESTS_DIR`.
+    The filenames do not have file extensions.
+
+    Returns
+    -------
+    list
+        A sorted list of filenames without file extensions.
+    """
+    res = set()
+    for _, _, filenames in os.walk(TESTS_DIR):
+        for filename in filenames:
+            res.add(filename.split(".")[0])
+    res = list(res)
+    res.sort()
+    return res
 
 
 def diff_lhs_rhs(v, i, il, io, rs, rsh, n, vth, ns):
