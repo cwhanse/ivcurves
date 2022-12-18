e6c7d910a6ca54ba0cf4db5af5457b2fa0e20854
Taos Transue <spacechips@rocketmail.com>
2022-12-18 08:27:56 -0800

    update elementary charge; increase test case string precision

--- ivcurves/utils.py
+++ ivcurves/utils.py
@@ -29,13 +29,22 @@ def set_globals():
 def constants():
     r"""
     Commonly used constants of the ivcurves scripts.
+
+    The Boltzmann's constant and electron/elementary charge are from [1]_.
+
+    References
+    ----------
+    .. [1] BIPM. Le Système international d’unités / The International System
+       of Units (‘The SI Brochure’).
+       Bureau international des poids et mesures, ninth edition, 2019.
+       URL https://www.bipm.org/en/publications/si-brochure, ISBN 978-92-822-2272-0.
     """
     num_pts = 100
     precision = 16
     atol = mp.mpmathify(1e-16)
 
     # Boltzmann's const (J/K), electron charge (C), temp (K)
-    k, q, temp_cell = map(mp.mpmathify, [1.380649e-23, 1.60217663e-19, 298.15])
+    k, q, temp_cell = map(mp.mpmathify, [1.380649e-23, 1.602176634e-19, 298.15])
     vth = (k * temp_cell) / q
 
     return {'k': k, 'q': q, 'temp_cell': temp_cell, 'vth': vth, 'atol': atol,
@@ -67,13 +76,17 @@ def mp_num_digits_left_of_decimal(num_mpf):
         # force mpf to string in decimal format, no scientific notation
         # mpf string will have precision*2 significant digits
         # all leading zeros are stripped
-        return mp.nstr(num_mpf, n=precision*2, min_fixed=-mp.inf,
-                       max_fixed=mp.inf).find('.')
+        res = mp.nstr(num_mpf, n=precision*2, min_fixed=-mp.inf,
+                      max_fixed=mp.inf).find('.')
+        if num_mpf < 0:
+            return res - 1 # ignore negative sign '-'
+        else:
+            return res
 
 
 def mp_nstr_precision_func(num_mpf):
     r"""
-    Converts an mpmath float to a string with 16 significant digits
+    Converts an mpmath float to a string with at least 16 significant digits
     after the decimal place.
 
     Parameters
@@ -84,18 +97,27 @@ def mp_nstr_precision_func(num_mpf):
     Returns
     -------
     str
-        A string representation of ``num_mpf`` with 16 significant digits
-        after the decimal place.
+        A string representation of ``num_mpf`` with at least 16 significant
+        digits after the decimal place.
     """
     precision = constants()['precision']
     ldigits = mp_num_digits_left_of_decimal(num_mpf)
-    return mp.nstr(num_mpf, n=ldigits+precision, strip_zeros=False)
+
+    # Stringifying to 16 significant digits truncates or rounds the mp.mpf
+    # value. The lost precision can cause error greater than 1e-16 when
+    # calculating the difference between the left and right side of the single
+    # diode equation. The number of required sigfigs in the string is increased
+    # by 3 to ensure the JSON test set values are still precise enough when
+    # converted into mp.mpf values. The required sigfigs may need to be
+    # increased when new JSON test sets are added.
+    sigfigs = ldigits + precision + 3
+    return mp.nstr(num_mpf, n=sigfigs, strip_zeros=False)
 
 
 def read_iv_curve_parameter_sets(filename):
     r"""
     Returns a dictionary of indices to a list of these values:
-    Index, photocurrent, saturation_current, resistance_series,
+    photocurrent, saturation_current, resistance_series,
     resistance_shunt, n, and cells_in_series.
     The indices and values are read from the CSV file at ``filename``.
 
@@ -116,7 +138,7 @@ def read_iv_curve_parameter_sets(filename):
         mapping = {}
         for row in reader:
             mapping[int(row['Index'])] = [mp.mpmathify(row[col])
-                                            for col in IV_PARAMETER_NAMES]
+                                          for col in IV_PARAMETER_NAMES]
         return mapping
 
 
@@ -131,7 +153,6 @@ def make_iv_curve_name(test_set_name, index):
     test_set_name : str
         The name of the test set that contains the test case of the IV curve's
         parameters.
-
     index : int
         The Index of the test case of the IV curve's parameters.
 
