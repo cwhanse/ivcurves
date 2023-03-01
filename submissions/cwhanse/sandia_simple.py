5a64ad9c444b9fa3dbe516b862f85a72c7d0ae7a
cwhanse <cwhanse@sandia.gov>
2023-03-01 15:25:57 -0700

    use code from Participating page

--- submissions/cwhanse/sandia_simple.py
+++ submissions/cwhanse/sandia_simple.py
@@ -12,44 +12,104 @@ References
 """
 
 import json
-from pathlib import Path
+import pathlib
 import numpy as np
 import pandas as pd
 import pvlib
 
 
-def get_test_set_filenames():
-    path_to_test_sets = Path('../../test_sets')
-    return [f'{path_to_test_sets}/{entry.stem}.json'
-            for entry in path_to_test_sets.iterdir()
-            if entry.is_file()]
+def get_test_set_filepaths():
+    """
+    Returns a sorted list of pathlib.Path objects pointing to the JSON test set
+    files. pathlib.Path objects can be passed directly to Python's ``open``
+    function to open the JSON file.
+
+    Returns
+    -------
+        A list.
+    """
+    path_to_test_sets = pathlib.Path.cwd() / '..' / '..' / 'test_sets'
+    file_entries = list({path_to_test_sets / f'{entry.stem}.json'
+                         for entry in path_to_test_sets.iterdir()
+                         if entry.is_file()})
+    file_entries.sort()
+    return file_entries
 
 
-def read_json(filepath):
+def get_test_set_name(filepath):
+    """
+    Gets a test set filename from a filepath.
+
+    Parameters
+    ----------
+    filepath : pathlib.Path
+        A filepath pointing to a JSON test set file.
+
+    Returns
+    -------
+        The test set name given a pathlib.Path object pointing to a JSON
+        test set file.
+    """
+    return filepath.stem
+
+
+def json_file_to_df(filepath):
+    """
+    Creates a pandas DataFrame from an IV Curve JSON file.
+    All of the numerical values stored as strings in the JSON as parsed to
+    np.float64.
+
+    Parameters
+    ----------
+    filepath : pathlib.Path
+        A Path object pointing to the JSON file.
+
+    Returns
+    -------
+        A pandas DataFrame.
+    """
+    curves_metadata = pd.read_json(filepath)
+    curves = pd.DataFrame(curves_metadata['IV Curves'].values.tolist())
+    curves['cells_in_series'] = curves_metadata['cells_in_series']
+
+    # set up index
+    curves['Index'] = curves['Index'].astype(int)
+    curves = curves.set_index('Index')
+
+    # convert Voltages, Currents, and diode_voltage from string arrays to
+    # float arrays. This truncates from precise values to 64-bit precision.
+    is_array = ['Voltages', 'Currents', 'diode_voltage']
+    curves[is_array] = curves[is_array].applymap(
+        lambda a: np.asarray(a, dtype=np.float64)
+    )
+
+    # convert from string to float
+    is_number = ['v_oc', 'i_sc', 'v_mp', 'i_mp', 'p_mp', 'Temperature']
+    curves[is_number] = curves[is_number].applymap(np.float64)
+
+    return curves
+
+
+def json_file_to_dict(filepath):
+    """
+    Returns a Python dict of the contents of a JSON file.
+
+    Parameters
+    ----------
+    filepath : pathlib.Path
+        The filepath pointing to a JSON file.
+
+    Returns
+    -------
+        A Python dict.
+    """
     with open(filepath, 'r') as file:
         return json.load(file)
 
 
-def json_to_df(filepath):
-    data = read_json(filepath)
-    df = pd.DataFrame.from_dict(data['IV Curves'], dtype=float)
-    # set up index
-    df['Index'] = df['Index'].astype(int)
-    df = df.set_index('Index')
-    # add cells_in_series to df
-    df['cells_in_series'] = data['cells_in_series']
-    # convert Voltages and Currents from string to float
-    for s in ['Voltages', 'Currents']:
-        df[s] = df[s].astype(object)
-    for idx in df.index:
-        for s in ['Voltages', 'Currents']:
-            df[s][idx] = [np.asarray(df[s][idx], dtype=np.float)]
-    return df
-
-
 if __name__ == "__main__":
 
-    test_files = get_test_set_filenames()
+    test_files = get_test_set_filepaths()
     
     # set up dataframe to hold results
     cols = ['photocurrent', 'saturation_current', 'resistance_series',
@@ -63,10 +123,10 @@ if __name__ == "__main__":
     
     for filen in test_files:
         # extract case name and make output file name
-        casename = Path(filen).name.strip('.json')
+        casename = pathlib.Path(filen).name.strip('.json')
         outname = casename + '.csv'
         # read IV curves for this case
-        data = json_to_df(filen)
+        data = json_file_to_df(filen)
         # set up Dataframe to contain each curve's diode equation parameters
         results = pd.DataFrame(index=data.index, columns=cols)
         # process each IV curve
@@ -88,6 +148,6 @@ if __name__ == "__main__":
             results.loc[d, 'cells_in_series'] = data.loc[d, 'cells_in_series']
     
     
-        outfilen = Path.cwd() / outname
+        outfilen = pathlib.Path.cwd() / outname
         with open(outfilen, 'w') as outfile:
             results.to_csv(outfile)
