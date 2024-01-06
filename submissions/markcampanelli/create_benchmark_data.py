fb71b9e5bd784fab9c7ed936e70ef2cff28fe238
Mark Campanelli <mark.campanelli@gmail.com>
2024-01-06 13:22:09 -0700

    Refactor a few small things

--- submissions/markcampanelli/create_benchmark_data.py
+++ submissions/markcampanelli/create_benchmark_data.py
@@ -1,8 +1,8 @@
 """Run unit test from pvfit package to generate fit data."""
 
-import pvfit.tests.modeling.dc.single_diode.equation.inference_test
+import pvfit.modeling.dc.single_diode.equation.inference_test
 import pytest
 
-test_file = pvfit.tests.modeling.dc.single_diode.equation.inference_test.__file__
-
-pytest.main(["-vvsx", "--cache-clear", "-k", "not test_fit_benchmark", test_file])
+if __name__ == "__main__":
+    test_file = pvfit.modeling.dc.single_diode.equation.inference_test.__file__
+    pytest.main(["-vvsx", "--cache-clear", "-k", "not test_fit_benchmark", test_file])
