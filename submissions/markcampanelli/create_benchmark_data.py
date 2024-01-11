1f0ec1da05062c263cfdaccd6ad3f0643768cc15
Mark Campanelli <mark.campanelli@gmail.com>
2024-01-11 08:27:05 -0700

    Update to latest commit on main and indicate data-generating test more explicitly

--- submissions/markcampanelli/create_benchmark_data.py
+++ submissions/markcampanelli/create_benchmark_data.py
@@ -5,4 +5,4 @@ import pytest
 
 if __name__ == "__main__":
     test_file = pvfit.modeling.dc.single_diode.equation.inference_test.__file__
-    pytest.main(["-vvsx", "--cache-clear", "-k", "not test_fit_benchmark", test_file])
+    pytest.main(["-vvsx", "--cache-clear", f"{test_file}::test_fit"])
